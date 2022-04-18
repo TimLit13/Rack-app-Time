5e5e9d2c2dbbd3eff0d111834530f99d04def75e
“TimLit13” <litidm@gmail.com>
2022-04-18 20:01:01 +0300

    Add functionality for rack app

--- app.rb
+++ app.rb
@@ -1,19 +1,50 @@
 class App
   def call(env)
-    [status, headers, body]
+    request = Rack::Request.new(env)
+
+    if request.get? && request.path == '/time'
+      formatted_time(request)
+    else 
+      page_not_found
+    end
   end
 
   private
 
-  def status
-    200
+  def formatted_time(request)
+    user_time_format = FormatTime.new(request.params['format'])
+    user_time_format.call
+
+    if user_time_format.errors_in_formats.any?
+      page_unknown_format(user_time_format.errors_in_formats)
+    else
+      page_user_format_time(user_time_format.time_now)
+    end
   end
 
-  def headers
-    {'Content-type' => 'text/plain'}
+  def page_not_found
+    status = 404
+    body = ["Page not found (#{status})"]
+    create_response(status, body)
   end
 
-  def body
-    ["Just for looking that it's work!\n"]
+  def page_unknown_format(errors)
+    status = 400
+    body = ["Unknown format #{errors.join} (#{status})"]
+    create_response(status, body)
+  end
+
+  def page_user_format_time(time_now)
+    status = 200
+    body = [time_now]
+    create_response(status, body)
+  end
+
+  def create_response(status, body)
+    response = Rack::Response.new
+    response.status = status
+    response['Content-Type'] = 'text/plain'
+    response.body = body
+    response.finish
   end
 end
---
+++ format_time.rb
@@ -0,0 +1,40 @@
+class FormatTime
+  TIME_FORMATS = { year:   "%Y", 
+                   month:  "%m", 
+                   day:    "%d",
+                   hour:   "%H", 
+                   minute: "%M", 
+                   second: "%S" }.freeze
+
+  attr_reader :errors_in_formats
+
+  def initialize(params)
+    @params = params
+  end
+
+  def call(*env)
+    query_string = parse_formats(@params)
+    @user_formats = []
+    @errors_in_formats = []
+
+    puts '*' * 20
+    puts query_string
+    puts '*' * 20
+    
+    query_string.each do |format|
+      format_sym = format.to_sym
+      TIME_FORMATS[format_sym] ? @user_formats.push(TIME_FORMATS[format_sym]) : @errors_in_formats.push(format)
+    end
+  end
+
+  def time_now
+    time_format = @user_formats.join('-')
+    Time.now.strftime(time_format)
+  end
+  
+  private
+
+  def parse_formats(query_string)
+    query_string.nil? ? Array.new : query_string.split(',')
+  end
+end

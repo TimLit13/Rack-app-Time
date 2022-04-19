64237e852a96895fb32457e968c80d57a34ea8c8
“TimLit13” <litidm@gmail.com>
2022-04-19 10:05:39 +0300

    Remove unuseful methods

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ class App
     if request.get? && request.path == '/time'
       formatted_time(request)
     else 
-      page_not_found
+      create_response(404, ["Page not found (404)"])
     end
   end
 
@@ -16,28 +16,10 @@ class App
     user_time_format.call
 
     if user_time_format.errors_in_formats.any?
-      page_unknown_format(user_time_format.errors_in_formats)
+      create_response(400, ["Unknown format #{user_time_format.errors_in_formats.join} (400)"])
     else
-      page_user_format_time(user_time_format.time_now)
+      create_response(200, [user_time_format.time_now])
     end
-  end
-
-  def page_not_found
-    status = 404
-    body = ["Page not found (#{status})"]
-    create_response(status, body)
-  end
-
-  def page_unknown_format(errors)
-    status = 400
-    body = ["Unknown format #{errors.join} (#{status})"]
-    create_response(status, body)
-  end
-
-  def page_user_format_time(time_now)
-    status = 200
-    body = [time_now]
-    create_response(status, body)
   end
 
   def create_response(status, body)
